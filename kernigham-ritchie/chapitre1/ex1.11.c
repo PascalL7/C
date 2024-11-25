67fb41e934df44f5333cc9a94d4efb435938ded4
Pascal Levallois <plvsjob@gmail.com>
2024-11-25 03:56:46 +0100

    Update ex1.11.c

--- kernigham-ritchie/chapitre1/ex1.11.c
+++ kernigham-ritchie/chapitre1/ex1.11.c
@@ -1,1 +1,7 @@
-
+#Tests
+- aucune saisie
+- saisie d'une ligne vide
+- saisie d'un espace
+- saisie d'une tabulation et d'un caractère
+- saisie d'un espace, de trois caractères et d'un saut de ligne
+- saisie de plusieurs caractères, d'espaces, de sauts de lignes suivis d'autres caractères
